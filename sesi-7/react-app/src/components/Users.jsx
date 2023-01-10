18c5f1ab4ed896794e80af5858fe90cd630d014b
Renal Apriansyah <renal.apr@gmail.com>
2023-01-10 21:07:35 +0700

    sesi 7 - class lifecycle updating-componentDidUpdate

--- sesi-7/react-app/src/components/Users.jsx
+++ sesi-7/react-app/src/components/Users.jsx
@@ -1,58 +1,94 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios'
 
 class Users extends React.Component {
   constructor() {
     super()
     console.log('constructor called')
     this.state = {
-      users: []
+      users: [],
+      user: {},
+      userId: 0
     }
   }
   
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.users.length != this.state.users.length) {
-      return true
-    }
+  setUserId = (id) => {
+    this.setState({
+      userId: id
+    })
+  }
+  
+  // shouldComponentUpdate(_, nextState) {
+  //   const isUsersChange = nextState.users.length != this.state.users.length
+  //   const isUserChange = nextState.user.id != this.state.user.id
+  //   if (isUsersChange || isUserChange) {
+  //     return true
+  //   }
     
-    return false
+  //   return false
+  // }
+  
+  componentDidUpdate(_, prevState) {
+    console.log('did update')
+    if(prevState.userId !== this.state.userId) {
+      axios.get(`https://jsonplaceholder.typicode.com/users/${this.state.userId}`)
+      .then(res => {
+        this.setState({
+          user: res.data
+        })
+      })
+    }
   }
   
   componentDidMount() {
     // fetch data from an api
     console.log(this.state.users, 'users did mount')
-    const users = [
-      {name: 'udin'},
-      {name: 'bambang'}
-    ] 
-    
-    setTimeout(() => {
-      console.log('try to set the user value')
-      this.setState({ users })
-    }, 2000);
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err, 'err')
+      })
   }
   
   render() {
+    console.log('%c users', 'color: #9BAC59', this.state)
     console.log(`%c render Users`, 'color: #9BAC59')
+    const user = this.state.user
     return (
       <>
         {
           this.state.users.length === 0 ?
           <>...Loading</>
           :
-          <div>
-            {
-            //  [
-            //   <div> <b>Name:</b> Udin </div>,
-            //   <div> <b>Name:</b> Bambang </div>
-            //  ]
-             this.state.users.map(user => {
-              return (
-                <div key={user.name}>
-                  <b>Name:</b> {user.name}
-                </div>
-              )
-            })
-            }
+          <div style={{ display: 'flex' }}>
+            <div style={
+              { width: '50%', borderRight: '1px solid'}}
+            >
+              <div>Name: {user.name}</div>
+              <div>Username: {user.username}</div>
+              <div>Email: {user.email}</div>
+              <div>Phone: {user.phone}</div>
+            </div>
+            <div>
+              {
+              //  [
+              //   <div> <b>Name:</b> Udin </div>,
+              //   <div> <b>Name:</b> Bambang </div>
+              //  ]
+                this.state.users.map(user => {
+                  return (
+                    <div key={user.name}>
+                      <b>Name:</b> {user.name} {' '}
+                      <button onClick={() => this.setUserId(user.id)}>Get Details</button>
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         }
       </>
