78c2965252fe52b4a2bfa386e94ee03093597b6a
Renal Apriansyah <renal.apr@gmail.com>
2023-01-12 20:57:33 +0700

    sesi 8 - styled component

--- sesi-8/react-app/src/components/Counter/index.jsx
+++ sesi-8/react-app/src/components/Counter/index.jsx
@@ -1,12 +1,42 @@
 import './styles.css'
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const blue = 'violet'
+const CounterMessageT = styled.div`
+ background-color: ${(props) => {
+  return props.bgColor
+ }};
+ padding: 10px 0px;
+`
+
+const CounterMessageO = styled.div((props) => ({
+  backgroundColor: props.bgColor,
+  padding: '10px 0px'
+}))
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0)
+  
+  const updateCounter = (n) => {
+    setCounter(counter + n)
+  }
+  
+  let counterMessageBgColor = 'lightgreen'
+  if(counter > 10) {
+    counterMessageBgColor = 'red'
+  } else if (counter < 0) {
+    counterMessageBgColor = 'yellow'
+  }
+  
   return (
     <div>
-      <div className='title'>My Counter</div>
-      <div>0</div>
-      <button>-</button> {' '}
-      <button>+</button>
+      <CounterMessageO bgColor={counterMessageBgColor}>
+        My Counter
+      </CounterMessageO>
+      <div>{counter}</div>
+      <button onClick={() => updateCounter(-1)} >-</button> {' '}
+      <button onClick={() => updateCounter(1)}>+</button>
     </div>
   )
 }
